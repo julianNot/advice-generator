fb74e53626f236bb1a72b3d7a3ac2f567ac8168d
Julian Ardila <julian.ardila@uptc.edu.co>
2022-08-02 16:19:28 -0500

    Desktop version

--- src/AdviceApp.jsx
+++ src/AdviceApp.jsx
@@ -1,13 +1,11 @@
 import { Advice } from "./components/Advice";
-import main from './assets/css/main.module.css';
+import main from "./assets/css/main.module.css";
 
 export const AdviceApp = () => {
 	return (
-        <section /* className={ main.container } */>
-            <div className={ main.card }>
-                <Advice className={ main.Advice} />
-                <button>Random i</button>
-            </div>
-        </section>
-    )
+		<div className={ main.card }>
+			<Advice className={ main.Advice } />
+			<button className={ main.btn }></button>
+		</div>
+	);
 };
--- src/components/Advice.jsx
+++ src/components/Advice.jsx
@@ -1,9 +1,15 @@
+import main from '../assets/css/main.module.css'
+import divider from '../assets/icons/pattern-divider-desktop.svg'
 
 export const Advice = () => {
+	const number = 117
+	const advice = "It is easy to sit up and take notice, what's difficult is getting up and taking action"
+
 	return (
-		<div>
-			<h2>Advice id: #</h2>
-			<p>concejo ....</p>
+		<div className={ main.text }>
+			<h2 className={ main.title }>ADVICE # { number }</h2>
+			<p className={ main.body }>" { advice } "</p>
+			<img className={ main.divider } src={ divider } alt="line separation" />
 		</div>
 	);
 };
